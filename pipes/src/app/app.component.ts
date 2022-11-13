94a032ba487cd47553353bb91d7ffefc8c18a0bc
Felipe Barcon <felipe.barcon@gmail.com>
2022-11-13 18:57:59 +0100

    opérateurs et pipes RxJs

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, filter, map, ReplaySubject } from 'rxjs';
+
+interface User {
+  firstName: string;
+  lastName: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'pipes';
+export class AppComponent implements OnInit {
+  public user: ReplaySubject<User> = new ReplaySubject(1);
+
+  ngOnInit() {
+    this.user.next({
+      firstName: 'Jean',
+      lastName: 'Dupuis',
+    });
+
+    const s1 = this.user
+      .pipe(
+        filter((user: User) => user != null), // la fonction passée à
+        // filter doit retourner un booléen
+        map((user: User) => `${user.firstName} ${user.lastName}`)
+      )
+      .subscribe((fullName: string) => console.log('[s1] : ', fullName));
+  }
 }
