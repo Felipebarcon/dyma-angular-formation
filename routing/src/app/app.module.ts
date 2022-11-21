29c4cef7a8176bdffa55e4bbc68afd8f7c17cbc9
Felipe Barcon <felipe.barcon@gmail.com>
2022-11-21 19:08:11 +0100

    adding routermodule and route paths

--- routing/src/app/app.module.ts
+++ routing/src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { UsersComponent } from './users/users.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const APP_ROUTES: Routes = [
+  {
+    path: '',
+    component: HomepageComponent,
+  },
+  {
+    path: 'users',
+    component: UsersComponent,
+  },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomepageComponent,
-    UsersComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomepageComponent, UsersComponent],
+  imports: [BrowserModule, RouterModule.forRoot(APP_ROUTES)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
