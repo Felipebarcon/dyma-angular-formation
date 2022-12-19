4493ff6cf28f18e7fdd80f3792a8562b33519f31
Felipe Barcon <felipe.barcon@gmail.com>
2022-12-19 22:01:18 +0100

    création d'un formulaire réactif

--- reactive-form/src/app/app.component.ts
+++ reactive-form/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'reactive-form';
+export class AppComponent implements OnInit {
+  public form: FormGroup = new FormGroup({
+    nom: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+  ngOnInit(): void {}
+
+  public submit(): void {
+    console.log(this.form);
+  }
 }
