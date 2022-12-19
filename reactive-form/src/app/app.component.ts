4995d3261c67cf56c6ece16de1270c454f2bdb3b
Felipe Barcon <felipe.barcon@gmail.com>
2022-12-20 00:52:57 +0100

    messages d'erreurs

--- reactive-form/src/app/app.component.ts
+++ reactive-form/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import {
   FormGroup,
   FormControl,
   Validators,
   AbstractControl,
+  AsyncValidatorFn,
+  ValidationErrors,
+  ValidatorFn,
 } from '@angular/forms';
+import { map, Observable, Subscription } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -12,53 +17,115 @@ import {
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  validatorPaul(formControl: AbstractControl): { [p: string]: boolean } | null {
-    if (formControl.value === 'paul') {
-      return { notPaul: true };
-    } else {
-      return null;
+  public form: FormGroup = new FormGroup(
+    {
+      name: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      email: new FormControl(
+        '',
+        [Validators.required, Validators.email],
+        this.asyncEmailValidator()
+      ),
+      confirmEmail: new FormControl('', Validators.email),
+    },
+    this.emailsMatch()
+  );
+  public subscription: Subscription = new Subscription();
+
+  public erreursForm: { [field: string]: string } = {
+    name: '',
+    email: '',
+    confirmEmail: '',
+    form: '',
+  };
+
+  public messagesErreur: { [field: string]: { [field: string]: string } } = {
+    name: {
+      required: 'Ce champ est requis.',
+      minlength: 'Vos nom et prénom doivent faire au moins 4 caractères.',
+    },
+    email: {
+      required: 'Entrez un email.',
+      email: 'Rentrez une adresse email valide.',
+      asyncEmailValidator: "L'email n'existe pas.",
+    },
+    confirmEmail: {
+      email: 'Rentrez une adresse email valide.',
+    },
+    form: {
+      noMatch: 'Les emails ne correspondent pas.',
+    },
+  };
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.subscription.add(
+      this.form.statusChanges.subscribe(() => {
+        this.changementStatusForm();
+      })
+    );
+  }
+
+  reinitialiser() {
+    this.form.reset();
+  }
+
+  submit() {
+    console.log(this.form.value);
+  }
+
+  emailsMatch(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      return control.get('email')!.value != control.get('confirmEmail')!.value
+        ? { noMatch: true }
+        : null;
+    };
+  }
+
+  asyncEmailValidator(): AsyncValidatorFn {
+    return (control: AbstractControl): Observable<ValidationErrors | null> => {
+      return this.http
+        .get(
+          `https://apilayer.net/api/check?access_key=115ce3ae2ccdef30e018edbde78d2c4a&email=${control.value}&smtp=1&format=1`
+        )
+        .pipe(
+          map((response: any) => {
+            console.log(response);
+            return !response.smtp_check
+              ? { asyncEmailValidator: control.value }
+              : null;
+          })
+        );
+    };
+  }
+
+  changementStatusForm() {
+    if (!this.form) {
+      return;
+    }
+    const form = this.form;
+    for (const field in this.erreursForm) {
+      this.erreursForm[field] = '';
+      let control: AbstractControl;
+      if (
+        field === 'form' &&
+        form.get('email')!.touched &&
+        form.get('confirmEmail')!.dirty
+      ) {
+        control = form;
+      } else {
+        control = form.get(field)!;
+      }
+      if (control && control.touched && control.invalid) {
+        const messages = this.messagesErreur[field];
+        for (const key in control.errors) {
+          this.erreursForm[field] += messages[key] + ' ';
+        }
+      }
     }
   }
 
-  validatorAsync(
-    formControl: AbstractControl
-  ): Promise<{ [s: string]: boolean } | null> {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(null);
-      }, 50000);
-    });
-  }
-  public form: FormGroup = new FormGroup({
-    nom: new FormControl('', [
-      this.validatorPaul,
-      Validators.required,
-      this.validatorAsync,
-    ]),
-    email: new FormControl(''),
-    password: new FormControl(''),
-  });
-
-  ngOnInit(): void {
-    // this.form.patchValue({
-    //   email: 'jean@live.fr',
-    // });
-    // this.form.setValue({
-    //   nom: 'Jean',
-    //   email: 'jean@live.fr',
-    //   password: '123',
-    // });
-    // this.form.addControl('nom', new FormControl('')); => ajouter un
-    // nouveau control
-    //
-    // this.form.removeControl('nom'); => supprimer un control
-    //
-    // this.form.setControl('nom', new FormControl()); => modifier un control
-    // console.log(this.form.get('nom'));
-  }
-
-  public submit(): void {
-    console.log(this.form);
-    // console.log(this.form.getRawValue());
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
