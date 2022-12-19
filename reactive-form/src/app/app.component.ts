ac4f763c6fce7a58e3239266b18467b91f9d865c
Felipe Barcon <felipe.barcon@gmail.com>
2022-12-19 23:04:24 +0100

    value and reset

--- reactive-form/src/app/app.component.ts
+++ reactive-form/src/app/app.component.ts
@@ -12,9 +12,28 @@ export class AppComponent implements OnInit {
     email: new FormControl(''),
     password: new FormControl(''),
   });
-  ngOnInit(): void {}
+
+  ngOnInit(): void {
+    // this.form.patchValue({
+    //   email: 'jean@live.fr',
+    // });
+    // this.form.setValue({
+    //   nom: 'Jean',
+    //   email: 'jean@live.fr',
+    //   password: '123',
+    // });
+    // this.form.addControl('nom', new FormControl('')); => ajouter un
+    // nouveau control
+    //
+    // this.form.removeControl('nom'); => supprimer un control
+    //
+    // this.form.setControl('nom', new FormControl()); => modifier un control
+    // console.log(this.form.get('nom'));
+  }
 
   public submit(): void {
-    console.log(this.form);
+    // console.log(this.form.value);
+    // console.log(this.form.getRawValue());
+    this.form.reset();
   }
 }
