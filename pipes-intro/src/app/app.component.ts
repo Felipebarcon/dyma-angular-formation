edae7376a735a8a3f20a104c80319c05782ca496
Felipe Barcon <felipe.barcon@gmail.com>
2022-12-26 21:18:45 +0100

    pipes local, date

--- pipes-intro/src/app/app.component.ts
+++ pipes-intro/src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'pipes-intro';
+  public nom: string = 'Jean';
+  public date1 = new Date();
+  public date2 = Date.now();
 }
--- pipes-intro/src/app/app.module.ts
+++ pipes-intro/src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import localeFr from '@angular/common/locales/fr';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeFr);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [{ provide: LOCALE_ID, useValue: 'fr-FR' }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
