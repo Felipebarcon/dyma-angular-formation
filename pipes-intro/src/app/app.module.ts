672856f42fa81e9a621141831cd5d2ec35167001
Felipe Barcon <felipe.barcon@gmail.com>
2022-12-26 21:37:05 +0100

    pipe personalisé

--- pipes-intro/src/app/app.module.ts
+++ pipes-intro/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import localeFr from '@angular/common/locales/fr';
 import { registerLocaleData } from '@angular/common';
+import { CbPipe } from './cb.pipe';
 registerLocaleData(localeFr);
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CbPipe],
   imports: [BrowserModule],
   providers: [{ provide: LOCALE_ID, useValue: 'fr-FR' }],
   bootstrap: [AppComponent],
