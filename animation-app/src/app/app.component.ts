f9697b30779ca485558d9e2878cb49b5af405937
Felipe Barcon <felipe.barcon@gmail.com>
2022-12-30 00:33:31 +0100

    function trigger state and style

--- animation-app/src/app/app.component.ts
+++ animation-app/src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { state, style, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('square', [
+      state(
+        'normal',
+        style({
+          backgroundColor: 'white',
+          border: '2px solid #444',
+          borderRadius: '0',
+        })
+      ),
+      state(
+        'wild',
+        style({
+          backgroundColor: 'red',
+          border: 'none',
+          borderRadius: '50%',
+        })
+      ),
+    ]),
+  ],
 })
 export class AppComponent {
+  public state = 'normal';
   title = 'animation-app';
 }
