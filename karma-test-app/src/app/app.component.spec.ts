405453f4eb89e108c0f863fa17bf4d43b42d1e5d
Felipe Barcon <felipe.barcon@gmail.com>
2022-12-30 23:37:10 +0100

    testing componants with dependencies as services

--- karma-test-app/src/app/app.component.spec.ts
+++ karma-test-app/src/app/app.component.spec.ts
@@ -1,11 +1,21 @@
 import { TestBed } from '@angular/core/testing';
+import { of, Observable } from 'rxjs';
 import { AppComponent } from './app.component';
+import { UsersService } from './users.service';
 
 describe('AppComponent', () => {
+  let userServiceSpy: any;
+
   beforeEach(async () => {
+    userServiceSpy = jasmine.createSpyObj('UserService', ['getUsers']);
+    userServiceSpy.getUsers.and.returnValue(of(['tintin']));
+
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
+      providers: [{ provide: UsersService, useValue: userServiceSpy }],
     }).compileComponents();
+
+    TestBed.inject(UsersService);
   });
 
   it('App tittle shoul be app', () => {
@@ -20,5 +30,13 @@ describe('AppComponent', () => {
     expect(h1Content).toEqual('Welcome testapp3');
 
     // console.log(fixture.nativeElement.querySelector('span').innerHTML);
+  });
+
+  it('Users should be initialized', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+
+    expect(userServiceSpy.getUsers).toHaveBeenCalled();
+    expect(fixture.componentInstance.users).toEqual(['tintin']);
   });
 });
--- karma-test-app/src/app/app.component.ts
+++ karma-test-app/src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'karma-test-app';
+  public users: string[];
+
+  constructor(private userService: UsersService) {}
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe((users) => (this.users = users));
+  }
 
   bonjour(): void {
     console.log('Bonjour');
